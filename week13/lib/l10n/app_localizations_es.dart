7d0933c1753b53e710ee9217a076a308872c889b
Qin YUE <emmayue@usc.edu>
2026-04-14 15:10:27 -0700

    chatv2

---
+++ week13/lib/l10n/app_localizations_es.dart
@@ -0,0 +1,21 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Spanish Castilian (`es`).
+class AppLocalizationsEs extends AppLocalizations {
+  AppLocalizationsEs([String locale = 'es']) : super(locale);
+
+  @override
+  String get appTitle => 'Mi App';
+
+  @override
+  String get helloWorld => 'Hola Mundo';
+
+  @override
+  String counterMessage(Object count) {
+    return 'Has hecho clic $count veces';
+  }
+}
